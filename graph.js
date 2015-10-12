c82e5de65a5ab650c6e6b4d6b62f3b82f5a54760
Alex Vasiu <alexandru.vasiu@cnglsibiu.ro>
2015-10-12 22:21:16 +0300

    Throw an exception at tree.eval() and some fixes

--- graph.js
+++ graph.js
@@ -2,26 +2,27 @@ var canvas = document.getElementById('myCanvas'),
 c= canvas.getContext('2d'),
 n=100,
 
-//define the math window
 xMin=-10,
 xMax=10,
 yMin=-10,
 yMax=10,
+
 math = mathjs(),
-            expr = '',
-            scope = {x: 0,
-			t:0
-			},
-            tree,
-
-			time=0,
-			timeIncrement=0.1;
-
-
-		canvas.style.left = "35%";
-        canvas.style.top = "25%";
-        canvas.style.position = "absolute";
-	//main program	
+       expr = '',
+       scope = {
+	      x: 0,
+		  t:0
+		},
+        tree,
+time=0,
+timeIncrement=0.1;
+		
+//canvas position:
+canvas.style.left = "35%";
+canvas.style.top = "25%";
+canvas.style.position = "absolute";
+	
+//main program:
 initExprFromHash();
 drawCurve();
 initTextField();
@@ -30,8 +31,8 @@ startAnimation();
 window.addEventListener('hashchange', initExprFromHash);		
 			
 function setExpr(newExpr){
-	expr=newExpr;
-	  tree = math.parse(expr, scope)
+	  expr=newExpr;
+	  tree = math.parse(expr, scope);
 }
 
 function initExprFromHash(){
@@ -56,34 +57,41 @@ function setHashFromExpr(){
 function drawCurve(){
 
 var i,xPixel,yPixel,
-percentX,percentY,
-mathX,mathY;
+    percentX,percentY,
+    mathX,mathY;
       //clear canvas
-	  c.clearRect(0,0,canvas.width,canvas.height);
+c.clearRect(0,0,canvas.width,canvas.height);
+
 c.beginPath();
+
 for(i=0;i<n;i++)
 		{
 		percentX= i/(n-1);
+		
 		mathX =percentX*(xMax-xMin)+xMin;
-
 		mathY=elvaluateMathExpr(mathX);
 
 		percentY=(mathY -yMin) /(yMax-yMin);
-
 		percentY=1-percentY;
 
 		xPixel=percentX*canvas.width;
 		yPixel=percentY*canvas.height;
+		
 		c.lineTo(xPixel,yPixel);
 		}
 		c.stroke();
 }
 
-	function elvaluateMathExpr(mathX){
+function elvaluateMathExpr(mathX){
 		scope.x=mathX;
 		scope.t=time;
-		return tree.eval();
-	}
+		try
+		{
+			return tree.eval();
+		}
+		catch(err)
+		{}
+}
 
 function initTextField(){  
     var input = $('#inputField');
@@ -94,24 +102,24 @@ function initTextField(){
 		  setExpr(input.val());
 		  setHashFromExpr();
       });
-	  }
+}
 
-	  function startAnimation(){
+function startAnimation(){
 
 		 (function animloop(){
   requestAnimationFrame(animloop);
   render();
-})();
-	  }
+         })();
+}
 function render(){
 			 time+=timeIncrement;
 			 drawCurve();
-		  }
+}
 
 function getHashValue(){
         return location.hash.substr(1);
-      }
+}
 
-      function setHashValue(value){
+function setHashValue(value){
         return location.hash = value;
-      }
+}
